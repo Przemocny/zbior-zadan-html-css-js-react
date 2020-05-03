bfbc773ed519934b5ad3f1e0750fd3569b95360b
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2020-05-03 19:38:39 +0200

    3.05.2020

---
+++ DOCS/JS/EventLoop.js
@@ -0,0 +1,6 @@
+// Bardzo często pojawiające się na rekrutacji pytanie - co to jest i jak działa Event Loop?
+
+// Wiem, że stary filmik, ale ten gość naprawdę dobrze to tłumaczy:
+
+// (26min)
+// https://www.youtube.com/watch?v=8aGhZQkoFbQ
