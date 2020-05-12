b1066fd46196aad4c6c54df516e5071c54b5e355
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2020-05-12 14:38:48 +0200

    12.05.2020

---
+++ REACT/RouterSelectFromStore.js
@@ -0,0 +1,16 @@
+
+// Stwórz Store Reduxa:
+// - store połączony z aplikacją, który zawiera array z itemami
+
+
+// Stwórz Router gdzie masz 2 routy:
+// - <ItemsListView> na  /items - pokazujący listę itemów
+// - <ItemDetailView> na  /items/${id} - pokazyjący jeden item
+
+
+// stwórz taką funkcjonalność w aplikacji, aby 
+// przy zmianie routa lub na GET `/items/${id}` 
+// wybierało ze store item o odpowiednim indexie
+// i podawało item do routa <ItemDetailView> w propie item
+
+
