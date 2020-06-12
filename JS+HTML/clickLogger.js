09fdeed06f268ad7720b9fc9d84a592efa85086c
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2020-06-12 15:54:50 +0200

    12.06.2020

---
+++ JS+HTML/clickLogger.js
@@ -0,0 +1,11 @@
+// stwórz funkcję clickLogger, która zamontowaniu
+
+// będzie logować do konsoli id oraz klasę elementu html, 
+// który został klikniety
+
+// funkcja clickLogger ma nasłuchiwać na wszystkie klikalne elementy, t
+// w obrębie całej strony
+
+function clickLogger(){
+
+}
