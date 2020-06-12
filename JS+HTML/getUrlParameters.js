7890415b5dc5308fb3c9a206d486f00d3bb28fb7
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2020-06-12 20:02:12 +0200

    12.06.2020

--- JS+HTML/getUrlParameters.js
+++ JS+HTML/getUrlParameters.js
@@ -1,6 +1,6 @@
 // stwórz funkcję, która po zamontowaniu będzie wracać wszystkie argumenty z adresu urla
 
 // dla const sample = url.com/post?page=10&id=1 
-// getUrlArguments(sample) będzie równy {page:10, id:1}
+// getUrlParameters(sample) będzie równy {page:10, id:1}
 
-function getUrlArguments(url){}
+function getUrlParameters(url){}
