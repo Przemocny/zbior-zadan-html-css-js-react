8f8c8cfc3f4b4af0b8eac7f5a68cd4e8d67283ea
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2020-06-16 19:44:28 +0200

    16.06.2020

---
+++ JS+HTML/StickyHeader.js
@@ -0,0 +1,13 @@
+// w zadaniu chodzi o wykonanie sticky header effect
+// jeśli header jest na górze strony, to jest wyższy oraz ma lity kolor
+// jeśli użytkownik przescrolluje ponad wysokość headera, header stanie się niższy oraz półprzeźroczysty
+
+// (zakładając, że podstawowa wysokość headera to 120px)
+// stwórz funkcję stickyHeader, która:
+// - przechwyci pozycję scrolla względem window
+// - jeśli wartosc scrolla będzie mniejsza niż wysokość elementu <header> to header będzie na górze strony, będzie miał lity kolor
+// - jeśli wartosc scrolla będzie większa niż 120px to header ma zmniejszyć wysokość do 60px i zostać "przyklejony" do górnej krawędzi strony w position fixed, oraz jego kolor ma zmienić alpha o -50%
+
+function stickyHeader(){
+
+}
