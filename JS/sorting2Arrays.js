e78a0ebbc670bbe6faa4de30ae5b3cc6ad0535c7
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2020-06-26 19:43:09 +0200

    26.06.2020

---
+++ JS/sorting2Arrays.js
@@ -0,0 +1,12 @@
+// masz zbiór A i B
+
+// A = [ ] // 40 losowych liczb od 1 do 50
+// B = [ ] // 10000 losowych liczb od 1 do 50
+
+// stwórz zbiór C, który w 5 różnych funkcjach
+
+// 1 - będzie zawierać tylko liczby pierwsze
+// 2 - będzie zawierać pierwsze 13 najczęściej występujących liczb w zbiorze B
+// 3 - w oparciu o zbiór A znajdzie brakujące liczby z zakresu od 1 do 50 i policzy ich ilość wystąpień w zbiorze B
+// 4 - na podstawie zbioru B znajdzie 10 najrzadziej występujących liczb podzielnych przez 3
+// 5 - stworzy obiekt, w którym unikatowe wartości ze zbioru A są kluczami tego obiektu, a ilość wystąpień tych liczb w zbiorze B jest wartościami tych kluczy 
