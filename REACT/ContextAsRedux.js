f4d5e4193f2bb29c625bcc1c05537067cbbfb6ce
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2020-07-07 19:25:46 +0200

    7.07.20

--- REACT/ContextAsRedux.js
+++ REACT/ContextAsRedux.js
@@ -1,5 +1,5 @@
 // Logika Reduxa w React składa się z 5 prostych elementów:
-// - struktury danych - tzn store
+// - struktury danych - tzn store/state - komórka pamięci z aktualnym stanem wszystkich danych
 // - reducera, który agreguje akcje i zmienia wg nich aktualny stan store
 // - akcji z dodatkowymi danymi wykonywanych na reducerze do modyfikacji store
 // - Providera - który określa zakres działania Reduxa
